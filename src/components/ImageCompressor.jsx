7dd33e9f1edf92247d1fb26ab6cfb9222a58a860
mik284 <mikeykamau222@gmail.com>
2022-10-04 16:43:48 +0300

    Deleted default files and Added imageCompressor component

---
+++ src/components/ImageCompressor.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import imageCompression from 'browser-image-compression'
+import Card from 'react-bootstrap/Card'
+
+
+function ImageCompressor() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ImageCompressor
